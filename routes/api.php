9a924eb9f4fc3b7d5282483231046c74e1a8a432
automaticko <automaticko@gmail.com>
2020-06-10 13:20:12 -0300

    Forces json response

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,11 @@
 
 use App\Http\Controllers\MutantController;
 use App\Http\Controllers\StatsController;
+use App\Http\Middleware\ForceJson;
+use Illuminate\Routing\Router;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::post('/mutant', [MutantController::class, 'store'])->name('store');
-Route::get('/stats', [StatsController::class, 'index'])->name('index');
+Route::group(['middleware' => ForceJson::class], function (Router $route) {
+    $route->post('/mutant', [MutantController::class, 'store'])->name('store');
+    $route->get('/stats', [StatsController::class, 'index'])->name('index');
+});
